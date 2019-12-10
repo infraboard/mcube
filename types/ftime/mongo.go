c20efff2fe65ace4b2b1a4eabaece554d91fbc51
yumaojun03 <18108053819@163.com>
2019-12-10 09:57:21 +0800

    添加sql

--- types/ftime/mongo.go
+++ types/ftime/mongo.go
@@ -1,1 +1,11 @@
 package ftime
+
+// MarshalBSON 实现JSON 序列化接口
+func (t Time) MarshalBSON() ([]byte, error) {
+	return t.MarshalJSON()
+}
+
+// UnmarshalBSON 实现JSON 反序列化接口
+func (t *Time) UnmarshalBSON(b []byte) error {
+	return t.UnmarshalJSON(b)
+}
