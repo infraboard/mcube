3a7b7fdb7eb91e224127f83b8b1b02b449c53eb8
yumaojun03 <18108053819@163.com>
2019-12-10 23:17:03 +0800

    修复时间戳

--- types/ftime/mongo.go
+++ types/ftime/mongo.go
@@ -5,10 +5,25 @@ import "go.mongodb.org/mongo-driver/bson/bsontype"
 // MarshalBSONValue implements the bson.ValueMarshaler interface.
 func (t Time) MarshalBSONValue() (bsontype.Type, []byte, error) {
 	data, err := t.MarshalJSON()
-	return bsontype.Int64, data, err
+
+	switch UsedFormatType {
+	case TIMESTAMP:
+		return bsontype.Int64, data, err
+	case TEXT:
+		return bsontype.String, data, err
+	default:
+		return bsontype.Int64, nil, ErrUnKnownFormatType
+	}
 }
 
 // UnmarshalBSONValue implements the bson.ValueUnmarshaler interface.
 func (t *Time) UnmarshalBSONValue(bt bsontype.Type, data []byte) error {
-	return t.UnmarshalJSON(data)
+	switch bt {
+	case bsontype.Int64:
+		return t.parseTS(string(data))
+	case bsontype.String:
+		return t.parseText(string(data))
+	default:
+		return ErrUnKnownFormatType
+	}
 }
