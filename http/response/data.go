6dbe640706d27142942eb026e1b4289e827cfafb
yumaojun03 <18108053819@163.com>
2020-01-19 20:32:55 +0800

    Page请求不在reponse中规范

--- http/response/data.go
+++ http/response/data.go
@@ -1,7 +1,5 @@
 package response
 
-import "github.com/infraboard/mcube/http/request"
-
 // Data to be used by controllers.
 type Data struct {
 	Code      *int        `json:"code"`                // 自定义返回码  0:表示正常
@@ -10,13 +8,6 @@ type Data struct {
 	Reason    string      `json:"reason,omitempty"`    // 异常原因
 	Message   string      `json:"message,omitempty"`   // 关于这次响应的说明信息
 	Data      interface{} `json:"data,omitempty"`      // 返回的具体数据
-}
-
-// PageData 数据分页数据
-type PageData struct {
-	*request.PageRequest
-	TotalCount uint        `json:"total"` // 总共多少条
-	List       interface{} `json:"list"`  // 页面数据
 }
 
 // NewData new实例
