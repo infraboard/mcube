7ce399fb64479bbbb6219496e93b8502aad9c52a
yumaojun03 <18108053819@163.com>
2020-04-22 14:00:32 +0800

    测试SQL builder

--- sqlbuilder/builder.go
+++ sqlbuilder/builder.go
@@ -5,54 +5,36 @@ import (
 	"strings"
 )
 
-// NewMySQLQuery new 实例
-func NewMySQLQuery(querySQL string) *Query {
+// NewQuery new 实例
+func NewQuery(querySQL string) *Query {
 	return &Query{
 		query:     querySQL,
-		whereOr:   []string{},
-		whereAnd:  []string{},
-		argsOr:    []interface{}{},
-		argsAnd:   []interface{}{},
-		argsLimit: []interface{}{},
+		whereStmt: []string{},
+		whereArgs: []interface{}{},
+		limitArgs: []interface{}{},
 	}
 }
 
 // Query 查询mysql数据库
 type Query struct {
 	query     string
-	whereOr   []string
-	whereAnd  []string
-	argsOr    []interface{}
-	argsAnd   []interface{}
-	limit     string
-	argsLimit []interface{}
+	whereStmt []string
+	whereArgs []interface{}
+	limitStmt string
+	limitArgs []interface{}
 	desc      string
 }
 
-// AddOrArg 添加参数
-func (q *Query) AddOrArg(v ...interface{}) {
-	q.argsOr = append(q.argsOr, v...)
-}
-
-// AddOrWhere 添加Where语句
-func (q *Query) AddOrWhere(v ...string) {
-	q.whereOr = append(q.whereOr, v...)
-}
-
-// AddAndArg 添加参数
-func (q *Query) AddAndArg(v ...interface{}) {
-	q.argsAnd = append(q.argsAnd, v...)
-}
-
-// AddAndWhere 添加Where语句
-func (q *Query) AddAndWhere(v ...string) {
-	q.whereAnd = append(q.whereAnd, v...)
+// Where 添加参数
+func (q *Query) Where(stmt string, v ...interface{}) {
+	q.whereStmt = append(q.whereStmt, stmt)
+	q.whereArgs = append(q.whereArgs, v...)
 }
 
 // Limit 这周Limit
 func (q *Query) Limit(offset int64, limit uint) {
-	q.limit = "LIMIT ?,? "
-	q.argsLimit = append(q.argsLimit, offset, limit)
+	q.limitStmt = "LIMIT ?,? "
+	q.limitArgs = append(q.limitArgs, offset, limit)
 }
 
 // Desc todo
@@ -61,31 +43,24 @@ func (q *Query) Desc(d string) {
 }
 
 func (q *Query) whereBuild() string {
-	if len(q.whereAnd) == 0 && len(q.whereOr) == 0 {
-		return ""
-	}
+	return "WHERE " + strings.Join(q.whereStmt, " AND ") + " "
+}
 
-	where := []string{}
-	if len(q.whereAnd) > 0 {
-		where = append(where, strings.Join(q.whereAnd, " AND "))
-	}
-	if len(q.whereOr) > 0 {
-		where = append(where, " ( "+strings.Join(q.whereOr, " OR ")+" ) ")
-	}
+// WhereArgs where 语句的参数
+func (q *Query) WhereArgs() []interface{} {
+	return q.whereArgs
+}
 
-	return "WHERE " + strings.Join(where, " AND ")
+// WhereStmt where条件列表
+func (q *Query) WhereStmt() []string {
+	return q.whereStmt
 }
 
 // Build 组件SQL
-func (q *Query) Build() string {
-	return q.query + q.whereBuild() + q.desc + q.limit + ";"
-}
+func (q *Query) Build() (stmt string, args []interface{}) {
+	stmt = q.query + " " + q.whereBuild() + q.desc + q.limitStmt + ";"
 
-// Args sql参数
-func (q *Query) Args() []interface{} {
-	args := []interface{}{}
-	args = append(args, q.argsAnd...)
-	args = append(args, q.argsOr...)
-	args = append(args, q.argsLimit...)
-	return args
+	args = append(args, q.whereArgs...)
+	args = append(args, q.limitArgs...)
+	return
 }
--- sqlbuilder/builder_test.go
+++ sqlbuilder/builder_test.go
@@ -1,1 +1,21 @@
 package sqlbuilder_test
+
+import (
+	"testing"
+
+	"github.com/infraboard/mcube/sqlbuilder"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestQueryBuild(t *testing.T) {
+	should := assert.New(t)
+	q := sqlbuilder.NewQuery("SELECT * FROM t")
+	q.Where("t.a = ? AND t.c = ? AND t.d LIKE ?", "one", "two", "three")
+	q.Desc("t.create_at")
+	q.Limit(0, 20)
+	stmt, args := q.Build()
+	should.Equal(stmt, "SELECT * FROM t WHERE t.a = ? AND t.c = ? AND t.d LIKE ? ORDER BY t.create_at DESC LIMIT ?,? ;")
+	should.Equal(args, []interface{}{"one", "two", "three", int64(0), uint(20)})
+	should.Equal(q.WhereStmt(), []string{"t.a = ? AND t.c = ? AND t.d LIKE ?"})
+	should.Equal(q.WhereArgs(), []interface{}{"one", "two", "three"})
+}
