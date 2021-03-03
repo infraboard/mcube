d2693f552e7dfac08829daf65a65a9a8a6247da5
yumaojun03 <18108053819@163.com>
2021-03-03 08:29:55 +0800

    修复测试用例问题

--- cmd/mcube/enum/generater_test.go
+++ cmd/mcube/enum/generater_test.go
@@ -9,7 +9,7 @@ import (
 
 func TestGenerate(t *testing.T) {
 	should := assert.New(t)
-	enum.G.SetSrcFile("../../../examples/enum.go")
+	enum.G.SetSrcFile("../../../examples/enum/enum.go")
 	code, err := enum.G.Generate()
 	t.Log(string(code))
 	should.NoError(err)
