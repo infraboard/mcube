6964607342ac4da45bfb5da75de6843104bf682c
yumaojun03 <18108053819@163.com>
2021-11-08 14:37:50 +0800

    添加app注册

---
+++ app/grpc.go
@@ -0,0 +1,56 @@
+package app
+
+import (
+	"fmt"
+
+	"google.golang.org/grpc"
+)
+
+// GRPCService GRPC服务的实例
+type GRPCApp interface {
+	Registry(*grpc.Server)
+	Config() error
+	Name() string
+}
+
+var (
+	grpcApps = map[string]GRPCApp{}
+)
+
+// RegistryService 服务实例注册
+func RegistryGrpcApp(app GRPCApp) {
+	// 已经注册的服务禁止再次注册
+	_, ok := grpcApps[app.Name()]
+	if ok {
+		panic(fmt.Sprintf("grpc app %s has registed", app.Name()))
+	}
+
+	grpcApps[app.Name()] = app
+}
+
+// LoadedGrpcApp 查询加载成功的服务
+func LoadedGrpcApp() []string {
+	return []string{}
+}
+
+func GetGrpcApp(name string) GRPCApp {
+	app, ok := grpcApps[name]
+	if !ok {
+		panic(fmt.Sprintf("grpc app %s not registed", name))
+	}
+
+	return app
+}
+
+// LoadGrpcApp 加载所有的Grpc app
+func LoadGrpcApp(server *grpc.Server) error {
+	for name, app := range grpcApps {
+		err := app.Config()
+		if err != nil {
+			return fmt.Errorf("config grpc app %s error %s", name, err)
+		}
+
+		app.Registry(server)
+	}
+	return nil
+}
---
+++ app/http.go
@@ -0,0 +1,58 @@
+package app
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/infraboard/mcube/http/router"
+)
+
+// HTTPService Http服务的实例
+type HTTPApp interface {
+	Registry(router.SubRouter)
+	Config() error
+	Name() string
+}
+
+var (
+	httpApps = map[string]HTTPApp{}
+)
+
+// RegistryHttpApp 服务实例注册
+func RegistryHttpApp(app HTTPApp) {
+	// 已经注册的服务禁止再次注册
+	_, ok := httpApps[app.Name()]
+	if ok {
+		panic(fmt.Sprintf("http app %s has registed", app.Name()))
+	}
+
+	httpApps[app.Name()] = app
+}
+
+// LoadedGrpcApp 查询加载成功的服务
+func LoadedHttpApp() []string {
+	return []string{}
+}
+
+func GetHttpApp(name string) HTTPApp {
+	app, ok := httpApps[name]
+	if !ok {
+		panic(fmt.Sprintf("http app %s not registed", name))
+	}
+
+	return app
+}
+
+// LoadHttpApp 装载所有的http app
+func LoadHttpApp(pathPrefix string, root router.Router) error {
+	for _, api := range httpApps {
+		if err := api.Config(); err != nil {
+			return err
+		}
+		if pathPrefix != "" && !strings.HasPrefix(pathPrefix, "/") {
+			pathPrefix = "/" + pathPrefix
+		}
+		api.Registry(root.SubRouter(pathPrefix))
+	}
+	return nil
+}
