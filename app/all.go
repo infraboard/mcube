2a8a3d36adcef940aa21778c3702debcb97688e2
yumaojun03 <18108053819@163.com>
2021-11-26 17:31:52 +0800

    修复初始化顺序问题

--- app/all.go
+++ app/all.go
@@ -1,13 +1,14 @@
 package app
 
 func InitAllApp() error {
-	for _, api := range grpcApps {
+	// 优先初始化内部app
+	for _, api := range internalApps {
 		if err := api.Config(); err != nil {
 			return err
 		}
 	}
 
-	for _, api := range internalApps {
+	for _, api := range grpcApps {
 		if err := api.Config(); err != nil {
 			return err
 		}
