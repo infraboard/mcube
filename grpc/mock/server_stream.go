e754a9a376b9403ac5ecd584baf15ea7fd1dd06a
yumaojun03 <18108053819@163.com>
2023-05-31 15:59:07 +0800

    补充mock

---
+++ grpc/mock/server_stream.go
@@ -0,0 +1,33 @@
+package mock
+
+import (
+	"context"
+
+	"google.golang.org/grpc/metadata"
+)
+
+func NewServerStreamBase() *ServerStreamBase {
+	return &ServerStreamBase{}
+}
+
+type ServerStreamBase struct {
+}
+
+func (i *ServerStreamBase) SetHeader(metadata.MD) error {
+	return nil
+}
+func (i *ServerStreamBase) SendHeader(metadata.MD) error {
+	return nil
+}
+func (i *ServerStreamBase) SetTrailer(metadata.MD) {
+
+}
+func (i *ServerStreamBase) Context() context.Context {
+	return nil
+}
+func (i *ServerStreamBase) SendMsg(m interface{}) error {
+	return nil
+}
+func (i *ServerStreamBase) RecvMsg(m interface{}) error {
+	return nil
+}
