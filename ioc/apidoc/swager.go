6a18705a64428236d54d5078422a63f795267013
yumaojun03 <18108053819@163.com>
2023-06-06 14:03:24 +0800

    补充 API Doc

--- ioc/apidoc/swager.go
+++ ioc/apidoc/swager.go
@@ -3,15 +3,14 @@ package apidoc
 import (
 	restfulspec "github.com/emicklei/go-restful-openapi/v2"
 	"github.com/emicklei/go-restful/v3"
-	"github.com/infraboard/mpaas/swagger"
 )
 
 // API Doc
-func APIDocs(apiDocPath string) *restful.WebService {
+func APIDocs(apiDocPath string, docs restfulspec.PostBuildSwaggerObjectFunc) *restful.WebService {
 	config := restfulspec.Config{
 		WebServices:                   restful.RegisteredWebServices(),
 		APIPath:                       apiDocPath,
-		PostBuildSwaggerObjectHandler: swagger.Docs,
+		PostBuildSwaggerObjectHandler: docs,
 		DefinitionNameHandler: func(name string) string {
 			if name == "state" || name == "sizeCache" || name == "unknownFields" {
 				return ""
