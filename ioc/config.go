3bdfc4820fd72a2c3a5fd0ad2afd72a94cfa6e23
yumaojun <18108053819@163.com>
2023-09-09 19:09:45 +0800

    补充mcube config

--- ioc/config.go
+++ ioc/config.go
@@ -10,23 +10,3 @@ func Config() ObjectStroe {
 		Namespace(configNamespace).
 		SetPriority(99)
 }
-
-// LoadConfigFromToml 从toml中添加配置文件, 并初始化全局对象
-// func LoadConfigFromFile(filePath string) error {
-// 	objects := store.Namespace(configNamespace)
-
-// 	errs := []string{}
-// 	objects.ForEach(func(o Object) {
-// 		cfg := map[string]Object{
-// 			o.Name(): o,
-// 		}
-// 		if _, err := toml.DecodeFile(filePath, cfg); err != nil {
-// 			errs = append(errs, err.Error())
-// 		}
-// 	})
-
-// 	if len(errs) > 0 {
-// 		return fmt.Errorf("%s", strings.Join(errs, ","))
-// 	}
-// 	return nil
-// }
