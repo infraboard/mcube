f323d1a3e7b9d4f70e2aa5e46f69f6cf1e163efc
yumaojun03 <719118794@qq.com>
2024-09-04 03:04:59 +0800

    处理set

--- types/set.go
+++ types/set.go
@@ -18,9 +18,10 @@ func (s *Set[T]) String() string {
 	return pretty.ToJSON(s)
 }
 
-func (s *Set[T]) Add(items ...T) {
+func (s *Set[T]) Add(items ...T) *Set[T] {
 	s.Items = append(s.Items, items...)
 	s.Total += int64(len(items))
+	return s
 }
 
 func (s *Set[T]) Len() int {
@@ -35,16 +36,29 @@ func (s *Set[T]) ForEach(h ItemHandleFunc[T]) {
 	}
 }
 
+type ItemMapFunc[T any] func(t T) T
+
+func (s *Set[T]) Map(m ItemMapFunc[T]) *Set[T] {
+	var mappedItems []T
+	for _, item := range s.Items {
+		mappedItems = append(mappedItems, m(item))
+	}
+	return &Set[T]{
+		Total: int64(len(mappedItems)),
+		Items: mappedItems,
+	}
+}
+
 type ItemFilterFunc[T any] func(t T) bool
 
-func (s Set[T]) Filter(f ItemFilterFunc[T]) Set[T] {
+func (s *Set[T]) Filter(f ItemFilterFunc[T]) *Set[T] {
 	var filteredItems []T
 	for _, item := range s.Items {
 		if f(item) {
 			filteredItems = append(filteredItems, item)
 		}
 	}
-	return Set[T]{
+	return &Set[T]{
 		Total: int64(len(filteredItems)),
 		Items: filteredItems,
 	}
