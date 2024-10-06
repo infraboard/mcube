74b3c66b1bb971005f8bf13607ec8a0d336ac8cf
yumaojun03 <719118794@qq.com>
2024-10-06 16:18:16 +0800

    补充restful日志

--- ioc/config/gorestful/framework.go
+++ ioc/config/gorestful/framework.go
@@ -1,6 +1,8 @@
 package gorestful
 
 import (
+	"time"
+
 	"github.com/emicklei/go-restful/v3"
 	"github.com/infraboard/mcube/v2/ioc"
 	"github.com/infraboard/mcube/v2/ioc/config/application"
@@ -13,7 +15,8 @@ import (
 
 func init() {
 	ioc.Config().Registry(&GoRestfulFramework{
-		Trace: true,
+		Trace:     true,
+		AccessLog: true,
 	})
 }
 
@@ -24,6 +27,8 @@ type GoRestfulFramework struct {
 
 	// 开启Trace
 	Trace bool `toml:"trace" json:"trace" yaml:"trace" env:"TRACE"`
+	//
+	AccessLog bool `toml:"access_log" json:"access_log" yaml:"access_log" env:"ACCESS_LOG"`
 }
 
 func (g *GoRestfulFramework) Priority() int {
@@ -45,8 +50,30 @@ func (g *GoRestfulFramework) Init() error {
 		g.log.Info().Msg("enable go-restful trace")
 		g.Container.Filter(otelrestful.OTelFilter(application.Get().GetAppName()))
 	}
+	// 补充AccessLog
+	if g.AccessLog {
+		g.Container.Filter(g.access_log())
+	}
 
 	// 注册给Http服务器
 	http.Get().SetRouter(g.Container)
 	return nil
+}
+
+func (g *GoRestfulFramework) access_log() restful.FilterFunction {
+	logger := log.Sub("access_log")
+	return func(req *restful.Request, resp *restful.Response, fc *restful.FilterChain) {
+		start := time.Now()
+
+		fc.ProcessFilter(req, resp)
+
+		// generate an access log message
+		logger.Info().Msgf("%-10s | %-15s | %-5d | %-10s | %s",
+			time.Since(start),
+			req.Request.RemoteAddr,
+			resp.StatusCode(),
+			req.Request.Method,
+			req.Request.URL.Path)
+
+	}
 }
