4e98c903fa7606a5728cc357e19c8ad5d08457a0
yumaojun03 <719118794@qq.com>
2024-12-23 22:30:45 +0800

    修复Cache Error

--- examples/cache/main.go
+++ examples/cache/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/infraboard/mcube/v2/ioc"
 	"github.com/infraboard/mcube/v2/ioc/config/cache"
 )
 
@@ -14,7 +16,9 @@ type TestStruct struct {
 func main() {
 	ctx := context.Background()
 
-	var v *TestStruct
+	ioc.DevelopmentSetupWithPath("etc/application.toml")
+
+	v := &TestStruct{}
 
 	// objectId --->  cached Objected
 	// 获取objectId对应的对象, 如果缓存中有则之间从缓存中获取, 如果没有 则通过提供的ObjectFinder直接获取
@@ -22,6 +26,7 @@ func main() {
 		return &TestStruct{Id: objectId, FiledA: "test"}, nil
 	}).Get("objectId", v)
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
 	}
+	fmt.Println(v)
 }
